2ab77e639962ccd29f32eb2518c1791de8fadac0
ENOSISBD\sayeed <sayeed.raihan@enosisbd.com>
2022-08-31 19:25:39 +0600

    Initial commit

---
+++ src/main/java/com/example/springdemo/SpringDemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("<h2>Hello %s!", name, "</h2>");
+    }
+}
